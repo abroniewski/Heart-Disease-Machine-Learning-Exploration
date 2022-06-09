2395f7c9b3ca359342aa46956bbea35a8e00bc7a
abroniewski <abroniewski@gmail.com>
2022-06-09 16:18:17 +0200

    Cleaning up - deleting draft files

--- src/raw-data-import.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/'
-r = requests.get(url, allow_redirects=True)
-
-download_location ='../data/raw'
-
-wget.download(url, download_location)
